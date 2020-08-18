ad21c89333f6b5c463f5ccdcb45d5a1c1ac732ca
vinarahmasari_program <43327988+Vina200601@users.noreply.github.com>
2020-08-18 08:31:29 +0700

    Initial commit

---
+++ app/src/main/java/com/example/latihan2anr/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.latihan2anr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+EditText edtNama;
+Button btnKlik;
+TextView txtHasil;
+String nama;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edtNama=(EditText)findViewById(R.id.edt_nama);
+        btnKlik=(Button)findViewById(R.id.Btn_Klik);
+        txtHasil=(TextView)findViewById(R.id.txt_hasil);
+
+
+        btnKlik.setOnClickListener(new View.OnClickListener() {
+@Override
+            public void onClick(View v) {
+                nama = edtNama.getText().toString();
+                txtHasil.setText(nama);
+            }
+        });
+    }
+}
